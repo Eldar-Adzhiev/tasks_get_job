20f2b39798f06c86a84cd2c8ef3336da30dff075
eldar <e.adzhiev93@gmail.com>
2023-04-01 12:39:03 +0300

    Добавлен тест

--- tests/test_crud_user.py
+++ tests/test_crud_user.py
@@ -1,6 +1,7 @@
 from api.user import Users
 from jsonschema import validate
 from api.schemas.crud_user import *
+from ui.pages.main_page import MainPage
 
 
 def test_create_user():
@@ -8,5 +9,12 @@ def test_create_user():
     response = user.create_user()
     validate(instance=response.json(), schema=create_user_schems)
     assert response.status_code == 201, "Incorrect status code"
+
+
+def test_compare_respons_ui_api(browser):
+    main_page = MainPage(browser)
+    main_page.open()
+    main_page.scroll_page_to_element(MainPage.CREATE_USER)
+    main_page.get_element(MainPage.CREATE_USER).click()
 
 

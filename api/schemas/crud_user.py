609389e294923401d6076e0992ca7e922d7f9095
eldar <e.adzhiev93@gmail.com>
2023-04-01 12:12:41 +0300

    Schemas

---
+++ api/schemas/crud_user.py
@@ -0,0 +1,13 @@
+create_user_schems = {
+
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "job": {"type": "string"},
+        "id": {"type": "string"},
+        "createdAt": {"type": "string"}
+
+    },
+    "required": ["name", "job", "id", "createdAt"]
+}
+

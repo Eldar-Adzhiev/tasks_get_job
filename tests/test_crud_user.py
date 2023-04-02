a5acd37846de2a83664c02743d817325e87b4d59
eldar <e.adzhiev93@gmail.com>
2023-04-02 12:56:55 +0300

    Тесты на получения и обновления пользователя

--- tests/test_crud_user.py
+++ tests/test_crud_user.py
@@ -2,7 +2,7 @@ import time
 
 import pytest
 
-from api.user import Users
+from api.user import Users, get_user
 from jsonschema import validate
 from api.schemas.crud_user import *
 from ui.pages.main_page import MainPage
@@ -33,10 +33,38 @@ def test_compare_respons_ui_api(browser):
     status_code_api = response.status_code
     body_api = response.json()
     assert status_code_api == int(status_code_ui), "Status codes not equal on ui and api. " \
-                                              f"ui = {status_code_ui}." \
-                                              f"api = {status_code_api}"
+                                                   f"ui = {status_code_ui}." \
+                                                   f"api = {status_code_api}"
     assert sorted(body_api.keys()) == sorted(body_ui.keys()), "Response body not equal. " \
-                                f"ui = {sorted(body_ui.keys())}. " \
-                                f"api = {sorted(body_api.keys())}"
+                                                              f"ui = {sorted(body_ui.keys())}. " \
+                                                              f"api = {sorted(body_api.keys())}"
 
 
+def test_get_user_by_id(create_user):
+    user = get_user(create_user["id"])
+    assert user, "User doesn't found "
+    assert user["name"] == create_user["name"], \
+        "User name incorrect. " \
+        f"Expected {create_user['name']} " \
+        f"Actual {user['name']}"
+
+
+@pytest.mark.parametrize("name, job", [
+    ("", ""),
+    ("AnotherName", "AnotherJob"),
+    (1234, 1243),
+    ("Name-with", "job-with"),
+    ("N_ame", "J_ob")
+])
+def test_update_user(create_user, name, job):
+    users = Users()
+    response = users.update_user(create_user["id"], name, job)
+    assert response.status_code == 200, "Incorrect status code " \
+                                        f"Expected = {200} " \
+                                        f"Actual = {response.status_code}"
+    assert response.json()["name"] == name, "Incorrect name. " \
+                                            f"Expected = {name} " \
+                                            f"Actual = {response.json()['name']}"
+    assert response.json()["job"] == job, "Incorrect name. " \
+                                          f"Expected = {job} " \
+                                          f"Actual = {response.json()['job']}"

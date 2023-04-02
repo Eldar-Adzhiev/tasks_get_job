86f38ea5641ca24d6a7b5691352c499315c0f4b3
eldar <e.adzhiev93@gmail.com>
2023-04-02 10:12:16 +0300

    Тест на сравнение статус кода и тела

--- tests/test_crud_user.py
+++ tests/test_crud_user.py
@@ -1,7 +1,10 @@
+import time
+
 from api.user import Users
 from jsonschema import validate
 from api.schemas.crud_user import *
 from ui.pages.main_page import MainPage
+import json
 
 
 def test_create_user():
@@ -14,7 +17,18 @@ def test_create_user():
 def test_compare_respons_ui_api(browser):
     main_page = MainPage(browser)
     main_page.open()
-    main_page.scroll_page_to_element(MainPage.CREATE_USER)
-    main_page.get_element(MainPage.CREATE_USER).click()
+    main_page.wait_element(MainPage.CREATE_USER).click()
+    status_code_ui = main_page.get_status_code().text
+    body_ui = json.loads(main_page.get_response_body().text)
+    user = Users()
+    response = user.create_user()
+    status_code_api = response.status_code
+    body_api = response.json()
+    assert status_code_api == int(status_code_ui), "Status codes not equal on ui and api. " \
+                                              f"ui = {status_code_ui}." \
+                                              f"api = {status_code_api}"
+    assert sorted(body_api.keys()) == sorted(body_ui.keys()), "Response body not equal. " \
+                                f"ui = {sorted(body_ui.keys())}. " \
+                                f"api = {sorted(body_api.keys())}"
 
 

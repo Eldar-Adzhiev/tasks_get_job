85b314d467ed8bec2554c9135323ee9beb0b0615
eldar <e.adzhiev93@gmail.com>
2023-04-07 22:14:06 +0300

    test

--- test_task.py
+++ test_task.py
@@ -1,17 +1,36 @@
-from pages.incoming_messages_page import IncomingMessages
 import time
+
 import allure
+
+from pages.base_page import BasePage
+from pages.main_page import MainPage
+from utils.config_parser import ConfigParser
+
 
 @allure.suite("Тестовое задание")
 def test_login_and_send_new_message(browser):
-    link = "https://mail.google.com/"
-    task = IncomingMessages(browser, link)
-    task.login()
-    count = task.get_count_of_messages()
-    task.click_write_new_message()
-    task.input_to()
-    task.input_count_of_message(count)
-    task.input_topic()
-    task.click_send()
-    time.sleep(5)
+    link = ConfigParser("config.json").get_config()["url"]
+    page = BasePage(browser, link)
+    page.open()
+    page.auth_page.login()
+    assert page.is_element_present(*MainPage.USER_NAME), "Main page don't open"
+    assert page.main_page.get_user_name().text == "Harry Potter", "Incorrect username."
 
+    date = page.get_date()
+    balance = page.fibonacci(date+1)
+    page.main_page.click_button_deposit()
+    assert page.main_page.is_field_text_present("Amount to be Deposited :"), "Field has incorrect label"
+
+    page.main_page.input_balance(balance)
+    page.main_page.click_button_submit_sum()
+    assert int(page.main_page.get_balance()) == balance, "Incorrect balance"
+    time.sleep(2)
+    page.main_page.click_button_withdraw()
+    assert page.main_page.is_field_text_present("Amount to be Withdrawn :"), "Field has incorrect label"
+
+    page.main_page.input_balance(balance)
+    page.main_page.click_button_submit_sum()
+    assert page.main_page.get_balance() == '0', "Incorrect balance"
+    time.sleep(2)
+    page.main_page.click_button_transactions()
+    time.sleep(15)
